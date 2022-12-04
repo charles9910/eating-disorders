a90a1ad6cde97aa36c57db5aca548f37b8e021aa
Charles <qiao2020@bu.edu>
2022-12-04 18:24:26 -0500

    change

---
+++ scripts/gamepage.js
@@ -0,0 +1,196 @@
+//References
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+//Questions and Options array
+
+const quizArray = [
+    {
+        id: "0",
+        question: "Mike hate some entire food grousp such as fruits and seafood, and he has a deeply rooted belief that if he eat apple, he will die.",
+        options: ["Selective Eating Disorder", "Night Eating Syndrome", "Binge Eating Disorder", "Pica"],
+        correct: "Selective Eating Disorder",
+    },
+    {
+        id: "1",
+        question: "Fiona is a pica patient, so which of the following might she consume?",
+        options: ["durian", "Canned fish", "Tissue", "Ewe's milk"],
+        correct: "Tissue",
+    },
+    {
+        id: "2",
+        question: "You've noticed something's not right with your roommate. He's always eating at a certain time. Which of the following times is when night eating disorder is likely to occur?",
+        options: ["9:00-10:00am", "2:00-3:00am", "12:00-13:00pm", "5:00-6:00pm"],
+        correct: "2:00-3:00am",
+    },
+    {
+        id: "3",
+        question: "Which of the following is not an eating disorder?",
+        options: ["stethalgia", "Night Eating Syndrome", "Binge Eating Disorder", "Pica"],
+        correct: "stethalgia",
+    },
+    {
+        id: "4",
+        question: "What do you need to do when you find out you have an eating disorder?",
+        options: ["As if nothing had happened", "Show off to your friends", "Take any medicine you want", "Go to see doctor"],
+        correct: "Go to see doctor",
+    },
+    {
+        id: "5",
+        question: "In addition to medical help, what else do people with eating disorders need？",
+        options: ["mathematics", "economics","psychological","architectonics"],
+        correct: "psychological",
+    }, 
+];
+
+//Restart Quiz
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+});
+
+//Next Button
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        //increment questionCount
+        questionCount += 1;
+        //if last question
+        if (questionCount == quizArray.length) {
+            //hide question container and display score
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+            //user score
+            userScore.innerHTML =
+                "Your score is " + scoreCount + " out of " + questionCount;
+        } else {
+            //display questionCount
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " of " + quizArray.length + " Question";
+            //display quiz
+            quizDisplay(questionCount);
+            count = 11;
+            clearInterval(countdown);
+            timerDisplay();
+        }
+    })
+);
+
+//Timer
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+        count--;
+        timeLeft.innerHTML = `${count}s`;
+        if (count == 0) {
+            clearInterval(countdown);
+            displayNext();
+        }
+    }, 1000);
+};
+
+//Display quiz
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+};
+
+//Quiz Creation
+function quizCreator() {
+    //randomly sort questions
+    quizArray.sort(() => Math.random() - 0.5);
+    //generate quiz
+    for (let i of quizArray) {
+        //randomly sort options
+        i.options.sort(() => Math.random() - 0.5);
+        //quiz card creation
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        //question number
+        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+        //question
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+        //options
+        div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+        quizContainer.appendChild(div);
+    }
+}
+
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+
+    //if user clicked answer == correct option stored in object
+    if (userSolution === quizArray[questionCount].correct) {
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+        //For marking the correct option
+        options.forEach((element) => {
+            if (element.innerText == quizArray[questionCount].correct) {
+                element.classList.add("correct");
+            }
+        });
+    }
+
+    //clear interval(stop timer)
+    clearInterval(countdown);
+    //disable all options
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+
+//initial setup
+function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 7;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+}
+
+//when user click on start button
+startButton.addEventListener("click", () => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+});
+
+//hide quiz and display start screen
+window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+};
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,16 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
 
-$(function(){
-    $(".coll_body").eq(0).show();
-    $(".Collapsing").click(function(){
-        $(this).toggleClass("current").siblings('.Collapsing').removeClass("current");//切换图标
-        $(this).next(".coll_body").slideToggle(500).siblings(".coll_body").slideUp(500);
-    });
+  // Scroll Reveal
+  // ScrollReveal().reveal('.services .grid .card');
+  
+  $('.square.blue').on('inview', function(event, isInView) {
+    if (isInView) {
+      // element is now visible in the viewport
+      $(this).addClass("in-view");
+    } else {
+      // element has gone out of viewport
+      $(this).removeClass("in-view");
+    }
+  });
 });
+ 
