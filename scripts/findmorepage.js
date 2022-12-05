d5eed4bbe67def77304bdfa89858254578d845f1
Charles <qiao2020@bu.edu>
2022-12-05 16:47:13 -0500

    changed

--- scripts/findmorepage.js
+++ scripts/findmorepage.js
@@ -1,7 +1,7 @@
 $(function(){
     $(".coll_body").eq().show();
     $(".Collapsing").click(function(){
-        $(this).toggleClass("current").siblings('.Collapsing').removeClass("current");//切换图标
+        $(this).toggleClass("current").siblings('.Collapsing').removeClass("current");
         $(this).next(".coll_body").slideToggle(500).siblings(".coll_body").slideUp(500);
     });
 });
